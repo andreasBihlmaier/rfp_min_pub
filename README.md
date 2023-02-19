e660a7461592345c00da33af3e0909349e5d0e8b
ahb <andreas.bihlmaier@gmx.de>
2023-02-19 21:06:17 +0100

    copy from upstream with examples_rclpy_minimal_publisher -> rfp_min_pub

---
+++ README.md
@@ -0,0 +1,8 @@
+Copy of https://github.com/ros2/examples/tree/humble/rclpy/topics/minimal_publisher as separate repo
+
+# Minimal "publisher" cookbook recipes
+
+This package contains a few different strategies for creating short nodes that blast out messages.
+The `publisher_old_school` recipe creates a talker node very similar to how it would be done in ROS 1 using rospy.
+The `publisher_local_function` recipe shows how to leverage the timers provided by ROS 2 to trigger message publication.
+The `publisher_member_function` recipe creates a class MinimalPublisher that sends messages periodically.
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+package_name = 'rfp_min_pub'
+
+setup(
+    name=package_name,
+    version='0.15.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Mikael Arguedas',
+    author_email='mikael@osrfoundation.org',
+    maintainer='Aditya Pande, Shane Loretz',
+    maintainer_email='aditya.pande@openrobotics.org, shane@openrobotics.org',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Examples of minimal publishers using rclpy.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'publisher_old_school = rfp_min_pub.publisher_old_school:main',
+            'publisher_local_function ='
+            ' rfp_min_pub.publisher_local_function:main',
+            'publisher_member_function ='
+            ' rfp_min_pub.publisher_member_function:main',
+        ],
+    },
+)
